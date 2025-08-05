a8b8195b519e2309e5ddbed3f9891c87964dd57c
mehakgmd <mehakgmd01@gmail.com>
2025-08-05 12:56:37 +0500

    second commit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,14 +6,14 @@ const SignIn = () => {
   return (
     <div className="relative min-h-screen flex items-center justify-center bg-gray-100 overflow-hidden">
       {/* Dotted background pattern */}
-      <div className="absolute top-24 left-96 w-32 h-32 opacity-70 pointer-events-none">
+      <div className="hidden md:block absolute top-24 left-96 w-32 h-32 opacity-70 pointer-events-none">
       <img 
       src='/yellow-dotted.svg'
       alt='Dots-Image'
       />
       </div>
       {/* Dotted background pattern */}
-      <div className="absolute bottom-24 right-96 w-32 h-32 opacity-70 pointer-events-none">
+      <div className="hidden md:block absolute bottom-24 right-96 w-32 h-32 opacity-70 pointer-events-none">
       <img 
       src='/blue-dotted.svg'
       alt='Dots-Image'
